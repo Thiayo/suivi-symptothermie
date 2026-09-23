7050da97d0660f71df340042eecc7e3a0e155a99
couple-et-sexualite <58232529+couple-et-sexualite@users.noreply.github.com>
2026-09-23 18:15:19 +0200

    Mettre à jour le cache PWA avec l’icône

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'symptothermie-shell-v2';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
+const CACHE_NAME = 'symptothermie-shell-v3';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
