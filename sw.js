3b9d2b57f9a0c4ae27a2eced5b7aa60482e9a908
couple-et-sexualite <58232529+couple-et-sexualite@users.noreply.github.com>
2026-09-23 19:46:01 +0200

    fix: cache all PWA install icons offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'symptothermie-shell-v3';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon.svg'];
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon.svg', './icons/icon-192.svg', './icons/icon-512.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
